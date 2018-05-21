9115972ccebf0436dc06db9d96e55dda62c4828b
Akash Biswas <akashbiswas.dipto@gmail.com>
2018-05-21 04:31:10 +0000

    task2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 /* JavaScript code here */
 console.log("External JS")
+
+function sumArray(arr){
+    var sum=0;
+    arr.forEach(function(value, index))
+    {
+        sum +=value;
+    });
+    return sum;
+}
+
+console.log(sumArray([1,2,3,4,5]));
