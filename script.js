790fbd306f0059269edbf286dbdaa4d5ff12652b
Akash Biswas <akashbiswas.dipto@gmail.com>
2018-05-21 04:40:42 +0000

    task2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* JavaScript code here */
 console.log("External JS")
 
+//task2
+
 function sumArray(arr){
     var sum=0;
     arr.forEach(function(value, index))
@@ -11,3 +13,14 @@ function sumArray(arr){
 }
 
 console.log(sumArray([1,2,3,4,5]));
+
+//task3
+
+function checkEmail(emailString){
+    var emailFormat=/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    var match= emailFormat.test(emailString);
+    return match;
+}
+
+console.log(checkEmail("john@smith.com"));
+console.log(checkEmail("johnsmith.com"));
