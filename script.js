6ab117c5b670b8c5f8959696e3cbb11bcb2ea491
Akash Biswas <akashbiswas.dipto@gmail.com>
2018-05-23 04:15:06 +0000

    tast5a

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* JavaScript code here */
-console.log("External JS")
+/* console.log("External JS")
 
 //task2
 
@@ -49,3 +48,30 @@ function getReadingStatus(index)
     
 }
 console.log(getReadingStatus(1));
+*/ 
+
+//task5
+
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+}
+var newItem={name: 'watch', price:64,quantity:1};
+addItem(newItem);
